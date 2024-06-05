07fdeb854507068a315d4b3e15ae09e0c9e35af3
GDEVHTCS <giorgioqromcheliscvili@gmail.com>
2024-06-06 01:07:54 +0400

    first commit

--- day10/homerwork.py
+++ day10/homerwork.py
@@ -23,3 +23,30 @@ print(sum) #დაბეჭდვა
 for i in range(1, 51):  #1 დან 50 ის ჩათვლით რიცხვები
     if i % 5 == 0: #5 ის ჯერადი რიცხვები
         print(i) #დაბეჭდვა
+
+#დავალება 6
+num1 = int(input("enter number :")) #შემოიტანეთ რიცხვი
+for i in range(1): #გამეორება for ციკლით ერთხელ
+    if 1000 < num1: #თუ ათასი ნაკლებია შემოტანილ რიცხვზე მაშინ უდიდესია
+        print('უდიდესი') #დაიბეჭდოს უდიდესი
+
+num2 = int(input("enter number :"))#რიცხვის შემოტანა
+for i in range(1): #გამეორება ერთხელ
+    if 1000 > num2: # თუ მეტია შემოტანილ რიცხვზე ათასი მაშინ უმცირესია
+        print('უმცირესი')
+
+#დავალება 7
+#ცვლადები - #num1 = 5 #print(num1)
+#ინფუთები: integer, string და float - #int(input("enter numb:))  #str(input("enter name:))         #float(input("enter numb:))
+#boolean - print(true and false)#true: print(false or false)#false
+#გამეორებები- for i in range():
+#if-elif-else: if i >= 30: 
+
+#დავალება 8
+product = 1 #ცვლადი
+for i in range(5, 11): #5 იდან 10-მე რიცხვები
+
+    product *= i #ნამრავლების გაკეთება
+print("ნამრავლები 5 იდან 10 ის ჩათვლით:", product) #ნამრავლი
+
+#დავალება 9
