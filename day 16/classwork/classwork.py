3713cc931c0c17779fc6e13a3f733e580e7efa38
GDEVHTCS <giorgioqromcheliscvili@gmail.com>
2024-06-18 19:10:52 +0400

    first commit

---
+++ day 16/classwork/classwork.py
@@ -0,0 +1,17 @@
+
+age = int(input("Enter your age: "))
+
+if age >= 5 and age < 13:
+    print("You are a child.")
+elif age >= 13 and age < 18:
+    print("You are a teenager.")
+else:
+    print("You are an adult.")
+
+film = ["uncharted, spiderman no way home, la brea "]
+
+song = ["pass the dutcite, fula-wamebi dros"]
+
+flsong1 = film + song
+
+print(flsong1)
---
+++ day 16/homework/homework.py
@@ -0,0 +1,17 @@
+fruits = ['apple', 'banana', 'cherry']
+print(fruits[0])  
+print(fruits[1]) 
+print(fruits[2])  
+
+numbers = [1, 2, 3, 4, 5]
+print(numbers[1:3]) 
+print(numbers[2:])  
+print(numbers[:3])   
+
+numbers = [1, 2, 3, 4, 5]
+numbers[1] = 'hello'
+print(numbers) 
+
+numbers = [1, 2, 3, 4, 5]
+print(numbers[-1])  
+print(numbers[-2:])   
