d9915532a585d283083e68ff2dcf10dc3ffa8d4e
GDEVHTCS <giorgioqromcheliscvili@gmail.com>
2024-06-25 23:23:55 +0200

    first commit

--- day 20/classwork/classwork.py
+++ day 20/classwork/classwork.py
@@ -2,8 +2,8 @@ def display_info():
     first_name = "giorgi"
     last_name = "oqromchedlishvili"
     age = 13
-    print("First Name: {first_name}")
-    print("Last Name: {last_name}")
-    print("Age: {age}")
+    print("First Name:",first_name)
+    print("Last Name:", last_name)
+    print("Age: age",age)
 
 display_info()
