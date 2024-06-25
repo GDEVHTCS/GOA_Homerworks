0271eb69bf2638d5848626236e3a2d132835d220
GDEVHTCS <giorgioqromcheliscvili@gmail.com>
2024-06-25 17:15:20 +0200

    first commit

---
+++ day 20/classwork/classwork.py
@@ -0,0 +1,9 @@
+def display_info():
+    first_name = "giorgi"
+    last_name = "oqromchedlishvili"
+    age = 13
+    print("First Name: {first_name}")
+    print("Last Name: {last_name}")
+    print("Age: {age}")
+
+display_info()
---
+++ day 20/homework/homework.py
@@ -0,0 +1,28 @@
+def hello():
+    print("GOA Best!")
+
+    def sum(a, b):
+         print(a + b)
+hello()
+
+def to_string(value):
+    print(str(value))
+
+def print_type(value):
+    print(type(value))
+
+def to_integer(value):
+    print(int(value))
+
+to_string(123)      
+to_string(45.67)    
+to_string(True)     
+
+print_type(123)     
+print_type("Hello")  
+print_type(3.14)     
+
+
+to_integer("123")    
+to_integer(45.67)    
+to_integer(True)     
